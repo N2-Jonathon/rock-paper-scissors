13175b353c1bae081fde2e2b756afe8004b0e9a5
Jonathon Quick <jonathon@spliff.li>
2022-10-18 23:15:31 +0200

    Added playRound function to script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+function playRound(playerSelection, computerSelection) {
+    var winner;
+    if(playerSelection === "rock") {
+        if(computerSelection === "paper") {
+            winner = "computer";
+        }
+        else if (computerSelection === "scissors") {
+            winner = "player";
+        }
+    }
+    else if(playerSelection === "paper") {
+        if(computerSelection === "scissors") {
+            winner = "computer";
+        }
+        else if (computerSelection === "rock") {
+            winner = "player";
+        }
+    }
+    else if(playerSelection === "scissors") {
+        if(computerSelection === "rock") {
+            winner = "computer";
+        }
+        else if (computerSelection === "paper") {
+            winner = "player";
+        }
+    }
+
+    if (winner === "player") {
+      return "You win! " + playerSelection + " beats " + computerSelection;
+    }
+    else if (winner === "computer") {
+      return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
